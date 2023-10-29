7248433166c4ce664ff73d3f07e0cc41dce70ba5
Semih Orhan <sorhan3.14@gmail.com>
2023-10-29 15:26:49 +0300

    change to async await for getting ganache accounts

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,16 +3,14 @@ const ganache = require('ganache');
 const { Web3 } = require('web3');
 
 const web3 = new Web3(ganache.provider());
+let accounts;
 
-// contract test code will go here
-beforeEach(() => {
-    web3.eth.getAccounts().then(fetchedAccounts => {
-        console.log(fetchedAccounts)
-    })
+beforeEach(async () => {
+    accounts = await web3.eth.getAccounts()
 })
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        
+        console.log(accounts);
     })
 })
