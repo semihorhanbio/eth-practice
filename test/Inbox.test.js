7d7023dadcb7ec111fe7de49d547067764916752
Semih Orhan <sorhan3.14@gmail.com>
2023-10-29 15:39:15 +0300

    test simple deployment

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,16 +1,21 @@
 const assert = require("assert")
 const ganache = require('ganache');
 const { Web3 } = require('web3');
+const { interface, bytecode } = require('../compile');
 
 const web3 = new Web3(ganache.provider());
 let accounts;
+let inbox;
 
 beforeEach(async () => {
     accounts = await web3.eth.getAccounts()
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi there!'] })
+        .send({ from: accounts[0], gas: '1000000' });
 })
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        console.log(accounts);
+        console.log(inbox);
     })
 })
