f18971df71a92d63ecff2f72571be3d43161d96f
Semih Orhan <sorhan3.14@gmail.com>
2023-10-29 15:20:51 +0300

    fetch accounts from ganache

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,6 +1,18 @@
+const assert = require("assert")
 const ganache = require('ganache');
 const { Web3 } = require('web3');
 
-// updated ganache and web3 imports added for convenience
+const web3 = new Web3(ganache.provider());
 
 // contract test code will go here
+beforeEach(() => {
+    web3.eth.getAccounts().then(fetchedAccounts => {
+        console.log(fetchedAccounts)
+    })
+})
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        
+    })
+})
