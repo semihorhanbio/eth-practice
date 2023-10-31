c2258db1c378a33c2579c3433ac6184deba840b6
Semih Orhan <sorhan3.14@gmail.com>
2023-10-31 08:47:59 +0300

    require manager to pick winner

--- lottery/contracts/Lottery.sol
+++ lottery/contracts/Lottery.sol
@@ -18,7 +18,10 @@ contract Lottery {
     }
 
     function pickWinner() public {
+        require(msg.sender == manager);
+
         uint index = random() % players.length;
         players[index].transfer(this.balance);
+        players = new address[](0);
     }
 }
