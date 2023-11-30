aa74a21aec1811053f64e6019cf391a53b07b4e3
Semih Orhan <sorhan3.14@gmail.com>
2023-11-30 14:12:54 +0300

    finish UI to pick winner for lottery

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -33,6 +33,18 @@ class App extends React.Component {
     this.setState({ message: "You have been entered!" });
   };
 
+  onClick = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    this.setState({ message: "Waiting on transaction success..." });
+
+    await lottery.methods.pickWinner().send({
+      from: accounts[0],
+    });
+
+    this.setState({ message: "A winner has been picked!" });
+  };
+
   render() {
     return (
       <div>
@@ -57,6 +69,12 @@ class App extends React.Component {
         </form>
 
         <hr />
+
+        <h4>Ready to pick a winner?</h4>
+        <button onClick={this.onClick}>Pick a winner!</button>
+
+        <hr />
+
         <h1>{this.state.message}</h1>
       </div>
     );
